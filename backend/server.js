44826f341c7c2304c4ac6aac288ae058a0ee696b
Aishwarya232006 <chhablaniaishwarya@gmail.com>
2025-11-04 12:20:47 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const { listingsRoute } = require("./modules/listings/listingsRoutes");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// ✅ FIX: Use connectDB as Express middleware
+// FIX: Use connectDB as Express middleware
 app.use(connectDB);
 
 // Application-level middlewares
