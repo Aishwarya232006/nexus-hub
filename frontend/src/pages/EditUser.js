8fa53e54273902e0a97dd6be4881f786f718e0f8
Aishwarya232006 <chhablaniaishwarya@gmail.com>
2025-12-06 16:39:45 -0500

    Create EditUser.js

---
+++ frontend/src/pages/EditUser.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserForm from '../components/Users/UserForm';
+
+const EditUser = () => {
+  return (
+    <div>
+      <UserForm />
+    </div>
+  );
+};
+
+export default EditUser;
