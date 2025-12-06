17b834d705bb31e3e19720988c6dada68c75b16b
Aishwarya232006 <chhablaniaishwarya@gmail.com>
2025-12-06 16:39:27 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+// Layout Components
+import NavigationBar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
+
+// Pages
+import Home from './pages/Home';
+import UsersPage from './pages/UsersPage';
+import ListingsPage from './pages/ListingsPage';
+import CreateUser from './pages/CreateUser';
+import CreateListing from './pages/CreateListing';
+import UserDetail from './pages/UserDetail';
+import ListingDetail from './pages/ListingDetail';
+import EditUser from './pages/EditUser';
+import EditListing from './pages/EditListing';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="d-flex flex-column min-vh-100">
+        <NavigationBar />
+        <Container className="flex-grow-1 py-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<UsersPage />} />
+            <Route path="/users/create" element={<CreateUser />} />
+            <Route path="/users/:id" element={<UserDetail />} />
+            <Route path="/users/edit/:id" element={<EditUser />} />
+            <Route path="/listings" element={<ListingsPage />} />
+            <Route path="/listings/create" element={<CreateListing />} />
+            <Route path="/listings/:id" element={<ListingDetail />} />
+            <Route path="/listings/edit/:id" element={<EditListing />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
