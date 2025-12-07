f5ec767165695a4087542fbaa2248d69aaa67c6d
Aishwarya232006 <chhablaniaishwarya@gmail.com>
2025-12-07 03:12:05 -0500

    Create ListingsPage.jsx

---
+++ frontend/src/pages/ListingsPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ListingList from '../components/Listings/ListingList';
+
+const ListingsPage = () => {
+  return <ListingList />;
+};
+
+export default ListingsPage;
