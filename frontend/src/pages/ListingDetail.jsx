5633739292b3256ede23330edd145e121253e723
Aishwarya232006 <chhablaniaishwarya@gmail.com>
2025-12-07 03:12:02 -0500

    Create ListingDetail.jsx

---
+++ frontend/src/pages/ListingDetail.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ListingDetail from '../components/Listings/ListingDetail';
+
+const ListingDetailPage = () => {
+  return <ListingDetail />;
+};
+
+export default ListingDetailPage;
