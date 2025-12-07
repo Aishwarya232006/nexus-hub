a53e210236c11ae59f94b8b234e09a4a4a8eeee6
Aishwarya232006 <chhablaniaishwarya@gmail.com>
2025-12-07 03:12:11 -0500

    Create EditListing.jsx

---
+++ frontend/src/pages/EditListing.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ListingForm from '../components/Listings/ListingForm';
+
+const EditListing = () => {
+  return <ListingForm />;
+};
+
+export default EditListing;
