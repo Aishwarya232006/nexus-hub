584fb51016245712b2ccdac885d0f96509e3b2d2
Aishwarya232006 <chhablaniaishwarya@gmail.com>
2025-12-07 03:11:59 -0500

    Create UserDetail.jsx

---
+++ frontend/src/pages/UserDetail.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import UserDetail from '../components/Users/UserDetail';
+
+const UserDetailPage = () => {
+  return <UserDetail />;
+};
+
+export default UserDetailPage;
