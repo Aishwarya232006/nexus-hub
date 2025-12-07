33074dfa040b2d60adc468f52f3509cc4ad28b5a
Aishwarya232006 <chhablaniaishwarya@gmail.com>
2025-12-07 03:10:43 -0500

    Create Navbar.jsx

---
+++ frontend/src/components/Layout/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+const NavigationBar = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" className="mb-4">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+           Nexus Hub
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/users">Users</Nav.Link>
+            <Nav.Link as={Link} to="/listings">Listings</Nav.Link>
+            <Nav.Link as={Link} to="/users/create">Create User</Nav.Link>
+            <Nav.Link as={Link} to="/listings/create">Create Listing</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavigationBar;
