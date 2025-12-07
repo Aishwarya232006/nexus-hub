6d96afdaae4a1239ae0334b82ac52d44053bc89c
Aishwarya232006 <chhablaniaishwarya@gmail.com>
2025-12-07 03:12:18 -0500

    Create CreateListing.jsx

---
+++ frontend/src/pages/CreateListing.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ListingForm from '../components/Listings/ListingForm';
+
+const CreateListing = () => {
+  return <ListingForm />;
+};
+
+export default CreateListing;
